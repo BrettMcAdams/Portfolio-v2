e80b324321b87d3b3c82229aeb428a3ce97f1905
unknown <brettmcadams96@gmail.com>
2021-02-02 17:29:35 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Portfolio
+My personal portfolio. Built with React.js
+
+<img src="https://user-images.githubusercontent.com/38120176/103391531-f435cf00-4ae7-11eb-864b-7f1ae0289747.png" alt="illustration of a rocket"/>
+
+## Features
+
+⚡️ Modern UI design \
+⚡️ Styled with Tailwind CSS\
+⚡️ Fully responsive and accessible design\
+⚡️ Lottie animation\
+⚡️ Awesome illustrations 
+
+
+### Illustrations
+
+- [https://illlustrations.co/ ](https://illlustrations.co/)
+- [https://www.pixeltrue.com/](https://www.pixeltrue.com/)
+
+### Author
+
+   Brett McAdams - https://www.theelipan.com/
---
+++ frontend/src/projectData.js
@@ -0,0 +1,28 @@
+const projects = [
+  {
+    title: 'Financial Tracker',
+    image: 'https://codingwithbrett.netlify.app/static/media/cheddar.171a269c.jpg',
+    description: 'Track your finances and develop a budget to help track your monthly spending. MERN Stack',
+    tools: ['React', 'Redux', 'MongoDB', 'JWT', 'Express'],
+    github: 'https://github.com/BrettMcAdams/cheddar-bank',
+    link: 'https://cheddarbank.netlify.app/',
+  },
+  {
+    title: 'Covid Tracker',
+    image: 'https://codingwithbrett.netlify.app/static/media/Covid.d571003b.jpg',
+    description: 'Covid tracker with interactive real time data display. API provided by disease.sh',
+    tools: ['React', 'API', 'Leaflet, Charts.js'],
+    github: 'https://github.com/BrettMcAdams/covid-tracker',
+    link: 'https://myworldcovidtracker.netlify.app/',
+  },
+  {
+    title: 'Swift Search',
+    image: 'https://codingwithbrett.netlify.app/static/media/swiftsearch.31ff58e6.jpg',
+    description: 'Swift is a powerful search engine powered by Googles search api that lets you explore anything on the web.',
+    tools: ['React', 'Firebase', 'Context API'],
+    github: 'https://github.com/Deelip7/RecipesFinder',
+    link: 'https://practical-archimedes-a61091.netlify.app/',
+  },
+];
+
+export default projects;
